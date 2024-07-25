93f79740127ab8826ac642cb67d9e39eeea957d0
srikalyani16 <81755038+srikalyani16@users.noreply.github.com>
2024-07-25 20:37:11 +0530

    Update dbConfig.js

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,8 +3,8 @@ const mongoose=require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.mongo_url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
     });
     console.log("MongoDB connected");
   } catch (error) {
